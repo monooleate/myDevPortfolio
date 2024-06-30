eb0e6162e5c7e462e11b69776d757de33d6e532a
János Mészáros <taskony@gmail.com>
2024-06-30 12:25:43 +0200

    Update ContactMe.jsx
    Correctly labeled form based on this site: https://dequeuniversity.com/rules/axe/4.9/label

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -57,21 +57,21 @@ const ContactMe = () => {
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-uni-text">
                             Your email
                         </label>
-                        <input type="email" name="user_email" className="shadow-sm bg-gray-200 border border-gray-300 text-sm text-left rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="janos@meszaros.com" required>
+                        <input type="email" id="email_address" name="user_email" className="shadow-sm bg-gray-200 border border-gray-300 text-sm text-left rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="janos@meszaros.com" required>
                         </input>
                     </div>
                     <div>
                         <label htmlFor="subject" className="block mb-2 text-sm font-medium text-uni-text ">
                             Subject
                         </label>
-                        <input type="text" name="subject" className="block p-3 w-full text-sm text-left bg-gray-2000 rounded-lg border bg-gray-200  border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 " placeholder="Interested in..." required>
+                        <input type="text" id="subject" name="subject" className="block p-3 w-full text-sm text-left bg-gray-2000 rounded-lg border bg-gray-200  border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 " placeholder="Interested in..." required>
                         </input>
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-uni-text ">
                             Your message
                         </label>
-                        <textarea name="message" rows="6" className="block p-2.5 w-full text-sm bg-gray-200  rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Let me know your thoughts...">
+                        <textarea name="message" id="message_body" rows="6" className="block p-2.5 w-full text-sm bg-gray-200  rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Let me know your thoughts...">
                         </textarea>
                     </div>
                     <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-uni-text rounded-lg bg-uni-fill sm:w-fit hover:bg-uni-palette focus:ring-4 focus:outline-none focus:ring-primary-300">
